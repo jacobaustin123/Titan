215327d904623945490f4989ae6f7f86a928c79a
Jacob Austin <ja3067@columbia.edu>
2018-05-25 17:36:17 -0400

    AUTO_COMMIT

--- include/vec.h
+++ include/vec.h
@@ -110,7 +110,7 @@ public:
         return Vec(v1.data[0] / v2.data[0], v1.data[1] / v2.data[1], v1.data[2] / v2.data[2]);
     } // two Vecs (elementwise)
 
-    friend std::ostream & operator << (std::ostream & strm, const Vec & v) {
+    CUDA_CALLABLE_MEMBER friend std::ostream & operator << (std::ostream & strm, const Vec & v) {
         return strm << "(" << v[0] << ", " << v[1] << ", " << v[2] << ")";
     } // print
 
