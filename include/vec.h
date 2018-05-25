08ed36f7b85d8ad3576a507497ae9e3dffe927f1
Jacob Austin <ja3067@columbia.edu>
2018-05-25 14:51:38 -0400

    ...

--- include/vec.h
+++ include/vec.h
@@ -18,33 +18,109 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <cuda.h>
+#include <iostream>
+#include <cmath>
 
 class Vec {
 public:
-    CUDA_CALLABLE_MEMBER Vec(); // default
-    CUDA_CALLABLE_MEMBER Vec(const Vec & v); // copy constructor
-    CUDA_CALLABLE_MEMBER Vec(double x, double y, double z); // initialization from x, y, and z values
-    CUDA_CALLABLE_MEMBER Vec & operator = (const Vec & v);
-    CUDA_CALLABLE_MEMBER Vec & operator += (const Vec & v);
-    CUDA_CALLABLE_MEMBER Vec operator - () const;
-    CUDA_CALLABLE_MEMBER double & operator [] (int n);
-    CUDA_CALLABLE_MEMBER const double & operator [] (int n) const;
+    CUDA_CALLABLE_MEMBER Vec() {
+        data[0] = 0;
+        data[1] = 0;
+        data[2] = 0;
+    } // default
 
-    CUDA_CALLABLE_MEMBER friend Vec operator + (const Vec & x, const Vec & y);
-    CUDA_CALLABLE_MEMBER friend Vec operator - (const Vec & x, const Vec & y);
+    CUDA_CALLABLE_MEMBER Vec(const Vec & v) {
+        data[0] = v.data[0];
+        data[1] = v.data[1];
+        data[2] = v.data[2];
+    } // copy constructor
 
-    CUDA_CALLABLE_MEMBER friend Vec operator * (const Vec & v, const double x); // double and Vec
-    CUDA_CALLABLE_MEMBER friend Vec operator * (const double x, const Vec & v);
-    CUDA_CALLABLE_MEMBER friend Vec operator * (const Vec & v1, const Vec & v2); // two Vecs (elementwise)
+    CUDA_CALLABLE_MEMBER Vec(double x, double y, double z) {
+        data[0] = x;
+        data[1] = y;
+        data[2] = z;
+    } // initialization from x, y, and z values
 
-    CUDA_CALLABLE_MEMBER friend Vec operator / (const Vec & v, const double x); // double and vec
-//    friend Vec operator / (const double x, const Vec & v); // not needed
-    CUDA_CALLABLE_MEMBER friend Vec operator / (const Vec & v1, const Vec & v2); // two Vecs (elementwise)
+    CUDA_CALLABLE_MEMBER Vec & operator=(const Vec & v) {
+        if (this == &v) {
+            return *this;
+        }
 
-    CUDA_CALLABLE_MEMBER friend std::ostream & operator << (std::ostream &, const Vec &); // print
+        data[0] = v.data[0];
+        data[1] = v.data[1];
+        data[2] = v.data[2];
 
-    CUDA_CALLABLE_MEMBER double norm() const; // gives vector norm
-    CUDA_CALLABLE_MEMBER double sum() const; // gives vector norm
+        return *this;
+    }
+
+    CUDA_CALLABLE_MEMBER Vec & operator+=(const Vec & v) {
+        data[0] += v.data[0];
+        data[1] += v.data[1];
+        data[2] += v.data[2];
+        return *this;
+    }
+
+    CUDA_CALLABLE_MEMBER Vec operator-() const{
+        return Vec(-data[0], -data[1], -data[2]);
+    }
+
+    CUDA_CALLABLE_MEMBER double & operator [] (int n) {
+        if (n < 0 || n >= 3) {
+            std::cerr << std::endl << "Out of bounds" << std::endl;
+            exit(EXIT_FAILURE);
+        } else {
+            return data[n];
+        }
+    }
+
+    CUDA_CALLABLE_MEMBER const double & operator [] (int n) const {
+        if (n < 0 || n >= 3) {
+            std::cerr << std::endl << "Out of bounds" << std::endl;
+            exit(EXIT_FAILURE);
+        } else {
+            return data[n];
+        }
+    }
+
+    CUDA_CALLABLE_MEMBER friend Vec operator+(const Vec & v1, const Vec & v2) {
+        return Vec(v1.data[0] + v2.data[0], v1.data[1] + v2.data[1], v1.data[2] + v2.data[2]);
+    }
+
+    CUDA_CALLABLE_MEMBER friend Vec operator-(const Vec & v1, const Vec & v2) {
+        return Vec(v1.data[0] - v2.data[0], v1.data[1] - v2.data[1], v1.data[2] - v2.data[2]);
+    }
+
+    CUDA_CALLABLE_MEMBER friend Vec operator*(const double x, const Vec & v) {
+        return Vec(v.data[0] * x, v.data[1] * x, v.data[2] * x);
+    }
+
+    CUDA_CALLABLE_MEMBER friend Vec operator*(const Vec & v, const double x) {
+        return x * v;
+    } // double and Vec
+
+    CUDA_CALLABLE_MEMBER friend Vec operator*(const Vec & v1, const Vec & v2) {
+        return Vec(v1.data[0] * v2.data[0], v1.data[1] * v2.data[1], v1.data[2] * v2.data[2]);
+    } // two Vecs (elementwise)
+
+    CUDA_CALLABLE_MEMBER friend Vec operator/(const Vec & v, const double x) {
+        return Vec(v.data[0] / x, v.data[1] / x, v.data[2] / x);
+    } // double and vec
+
+    CUDA_CALLABLE_MEMBER friend Vec operator/(const Vec & v1, const Vec & v2) {
+        return Vec(v1.data[0] / v2.data[0], v1.data[1] / v2.data[1], v1.data[2] / v2.data[2]);
+    } // two Vecs (elementwise)
+
+    CUDA_CALLABLE_MEMBER friend std::ostream & operator << (std::ostream & strm, const Vec & v) {
+        return strm << "(" << v[0] << ", " << v[1] << ", " << v[2] << ")";
+    } // print
+
+    CUDA_CALLABLE_MEMBER double norm() const {
+        return sqrt(pow(data[0], 2) + pow(data[1], 2) + pow(data[2], 2));
+    } // gives vector norm
+
+    CUDA_CALLABLE_MEMBER double sum() const {
+        return data[0] + data[1] + data[2];
+    } // gives vector norm
 
 private:
     double data[3] = { 0 }; // initialize data to 0
--- src/vec.cu
+++ src/vec.cu
@@ -7,112 +7,112 @@
 //
 
 #include "vec.h"
-#include <iostream>
-#include <cmath>
 
-Vec::Vec() {
-    data[0] = 0;
-    data[1] = 0;
-    data[2] = 0;
-}
-
-Vec::Vec(double x, double y, double z) {
-    data[0] = x;
-    data[1] = y;
-    data[2] = z;
-}
-
-Vec::Vec(const Vec & v) {
-    data[0] = v.data[0];
-    data[1] = v.data[1];
-    data[2] = v.data[2];
-}
-
-Vec & Vec::operator=(const Vec & v) {
-    if (this == &v) {
-        return *this;
-    }
-
-    data[0] = v.data[0];
-    data[1] = v.data[1];
-    data[2] = v.data[2];
-
-    return *this;
-}
-
-Vec & Vec::operator+=(const Vec & v) {
-    data[0] += v.data[0];
-    data[1] += v.data[1];
-    data[2] += v.data[2];
-    return *this;
-}
-
-Vec Vec::operator-() const{
-    return Vec(-data[0], -data[1], -data[2]);
-}
-
-
-Vec operator+(const Vec & v1, const Vec & v2) {
-    return Vec(v1.data[0] + v2.data[0], v1.data[1] + v2.data[1], v1.data[2] + v2.data[2]);
-}
-
-Vec operator-(const Vec & v1, const Vec & v2) {
-    return Vec(v1.data[0] - v2.data[0], v1.data[1] - v2.data[1], v1.data[2] - v2.data[2]);
-}
-
-Vec operator*(const double x, const Vec & v) {
-    return Vec(v.data[0] * x, v.data[1] * x, v.data[2] * x);
-}
-
-Vec operator*(const Vec & v, const double x) {
-    return x * v;
-}
-
-Vec operator*(const Vec & v1, const Vec & v2) {
-    return Vec(v1.data[0] * v2.data[0], v1.data[1] * v2.data[1], v1.data[2] * v2.data[2]);
-}
-
-Vec operator/(const Vec & v, const double x) {
-    return Vec(v.data[0] / x, v.data[1] / x, v.data[2] / x);
-}
-
-//Vec operator/(const double x, const Vec & v) {
-//    return x / v;
+//Vec::Vec() {
+//    data[0] = 0;
+//    data[1] = 0;
+//    data[2] = 0;
 //}
-
-Vec operator/(const Vec & v1, const Vec & v2) {
-    return Vec(v1.data[0] / v2.data[0], v1.data[1] / v2.data[1], v1.data[2] / v2.data[2]);
-}
-
-std::ostream & operator << (std::ostream & strm, const Vec & v) {
-    return strm << "(" << v[0] << ", " << v[1] << ", " << v[2] << ")";
-}
-
-double & Vec::operator [] (int n) {
-    if (n < 0 || n >= 3) {
-        std::cerr << std::endl << "Out of bounds" << std::endl;
-        exit(EXIT_FAILURE);
-    } else {
-        return data[n];
-    }
-}
-
-const double & Vec::operator [] (int n) const {
-    if (n < 0 || n >= 3) {
-        std::cerr << std::endl << "Out of bounds" << std::endl;
-        exit(EXIT_FAILURE);
-    } else {
-        return data[n];
-    }
-}
-
-double Vec::norm() const {
-    return sqrt(pow(data[0], 2) + pow(data[1], 2) + pow(data[2], 2));
-}
-
-double Vec::sum() const {
-    return data[0] + data[1] + data[2];
-}
+//
+//Vec::Vec(const Vec & v) {
+//    data[0] = v.data[0];
+//    data[1] = v.data[1];
+//    data[2] = v.data[2];
+//}
+//
+//Vec::Vec(double x, double y, double z) {
+//    data[0] = x;
+//    data[1] = y;
+//    data[2] = z;
+//}
+//
+//
+//
+//Vec & Vec::operator=(const Vec & v) {
+//    if (this == &v) {
+//        return *this;
+//    }
+//
+//    data[0] = v.data[0];
+//    data[1] = v.data[1];
+//    data[2] = v.data[2];
+//
+//    return *this;
+//}
+//
+//Vec & Vec::operator+=(const Vec & v) {
+//    data[0] += v.data[0];
+//    data[1] += v.data[1];
+//    data[2] += v.data[2];
+//    return *this;
+//}
+//
+//Vec Vec::operator-() const{
+//    return Vec(-data[0], -data[1], -data[2]);
+//}
+//
+//
+//Vec operator+(const Vec & v1, const Vec & v2) {
+//    return Vec(v1.data[0] + v2.data[0], v1.data[1] + v2.data[1], v1.data[2] + v2.data[2]);
+//}
+//
+//Vec operator-(const Vec & v1, const Vec & v2) {
+//    return Vec(v1.data[0] - v2.data[0], v1.data[1] - v2.data[1], v1.data[2] - v2.data[2]);
+//}
+//
+//Vec operator*(const double x, const Vec & v) {
+//    return Vec(v.data[0] * x, v.data[1] * x, v.data[2] * x);
+//}
+//
+//Vec operator*(const Vec & v, const double x) {
+//    return x * v;
+//}
+//
+//Vec operator*(const Vec & v1, const Vec & v2) {
+//    return Vec(v1.data[0] * v2.data[0], v1.data[1] * v2.data[1], v1.data[2] * v2.data[2]);
+//}
+//
+//Vec operator/(const Vec & v, const double x) {
+//    return Vec(v.data[0] / x, v.data[1] / x, v.data[2] / x);
+//}
+//
+////Vec operator/(const double x, const Vec & v) {
+////    return x / v;
+////}
+//
+//Vec operator/(const Vec & v1, const Vec & v2) {
+//    return Vec(v1.data[0] / v2.data[0], v1.data[1] / v2.data[1], v1.data[2] / v2.data[2]);
+//}
+//
+//std::ostream & operator << (std::ostream & strm, const Vec & v) {
+//    return strm << "(" << v[0] << ", " << v[1] << ", " << v[2] << ")";
+//}
+//
+//double & Vec::operator [] (int n) {
+//    if (n < 0 || n >= 3) {
+//        std::cerr << std::endl << "Out of bounds" << std::endl;
+//        exit(EXIT_FAILURE);
+//    } else {
+//        return data[n];
+//    }
+//}
+//
+//const double & Vec::operator [] (int n) const {
+//    if (n < 0 || n >= 3) {
+//        std::cerr << std::endl << "Out of bounds" << std::endl;
+//        exit(EXIT_FAILURE);
+//    } else {
+//        return data[n];
+//    }
+//}
+//
+//double Vec::norm() const {
+//    return sqrt(pow(data[0], 2) + pow(data[1], 2) + pow(data[2], 2));
+//}
+//
+//double Vec::sum() const {
+//    return data[0] + data[1] + data[2];
+//}
 
 double dot(const Vec & a, const Vec & b) {
     return (a * b).sum();
