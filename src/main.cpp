7bd7e98be714005e5e4b19cac378c52d45cb2351
Jacob Austin <ja3067@columbia.edu>
2018-06-04 23:37:04 -0400

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,51 @@
 #include <stdlib.h>
 #include <cmath>
 
-// Include GLEW
-#include <GL/glew.h>
-
-// Include GLFW
-#include <GLFW/glfw3.h>
-
-// Include GLM
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-using namespace glm;
-
-// Include shaders and support files
-#include "common/shader.h"
 #include "vec.h"
 #include "sim.h"
 
-#include "graphics.h"
 
 static Simulation sim;
 
-// #cmakedefine SOURCE_DIR "${SOURCE_DIR}"
-
 void translateMass() {
     ContainerObject * o = sim.getObject(0);
-    o -> translate(3 * Vec(cos(sim.time()), sin(sim.time()), 0));
-}
-
-
-void doStuff() { // user defined function
-    Mass * m1 = sim.getMass(0);
-    m1 -> setPos(Vec(0.1, 0.1, 0.1));
+    o->translate(Vec(cos(sim.time()), sin(sim.time()), 0));
 }
 
 int main()
 {
-    Cube * c = sim.createCube(Vec(0, 0, 10), 2.0); // create Cube object centered at (0, 0, 10) with side length 2.0
+//    Simulation sim; // initialize simulation object
+
+    Cube * c = sim.createCube(Vec(2, -2, 5), 2); // create Cube object centered at (0, 0, 10) with side length 2.0
     c -> setKValue(1000); // set the spring constant for all springs to 10
-    c -> setMassValue(1.0); // set all masses to 1.0
-    c -> setDeltaTValue(0.00001); // set the dt value for all masses in the cube to 0.00005
+    c -> setMassValue(1.0); // set all masses to 2.0
+    c -> setDeltaTValue(0.00001); // set the dt value for all masses in the cube to 0.00001
 
     for (Spring * s : c -> springs) {
         s -> setRestLength((s -> _right->getPosition() - s -> _left->getPosition()).norm());
     }
+    
+    Cube * c1 = sim.createCube(Vec(-2, 2, 5), 2); // create Cube object centered at (0, 0, 10) with side length 2.0
+    c1 -> setKValue(1000); // set the spring constant for all springs to 10
+    c1 -> setMassValue(1.0); // set all masses to 2.0
+    c1 -> setDeltaTValue(0.00001); // set the dt value for all masses in the cube to 0.00001
 
-    Plane * p = sim.createPlane(Vec(0, 0, 1), 0); // add a constraint (can't go below plane z = 0)
+    for (Spring * s : c1 -> springs) {
+        s -> setRestLength((s -> _right->getPosition() - s -> _left->getPosition()).norm());
+    }
+    
+    sim.createPlane(Vec(0, 0, 1), 0);
 
-    sim.runFunc(translateMass, 0, 2.5); // schedule function to run at time 0, repeat every 2.5 s
+    sim.setBreakpoint(0.01);
+    sim.runFunc(translateMass, 1.0);
 
-    sim.setBreakpoint(20000); // set breakpoint (could be end of program or just time to check for updates)
     sim.run();
+
+    while ( sim.time() < 10.0 ) {
+        sim.printPositions();
+        sim.setBreakpoint(sim.time() + 0.7);
+        sim.resume();
+    }
 
     return 0;
 }
