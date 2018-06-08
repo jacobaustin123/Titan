ae683709edf66cee9d977aabd4f8cb7cbbd8079f
Jacob Austin <ja3067@columbia.edu>
2018-06-07 21:55:19 -0400

    updated rendering, objects

--- src/main.cpp
+++ src/main.cpp
@@ -20,27 +20,28 @@ int main()
 //    Cube * c2 = sim.createCube(Vec(-2, 2, 5), 2); // create Cube object centered at (0, 0, 10) with side length 2.0
 //    Cube * c3 = sim.createCube(Vec(2, 2, 5), 2); // create Cube object centered at (0, 0, 10) with side length 2.0
 //    Cube * c4 = sim.createCube(Vec(-2, -2, 5), 2); // create Cube object centered at (0, 0, 10) with side length 2.0
-//
-    Lattice * l1 = sim.createLattice(Vec(0, 0, 5), Vec(5, 5, 2), 5, 5, 3);
 
-    sim.setSpringConstant(10000);
+    Lattice * l1 = sim.createLattice(Vec(0, 0, 5), Vec(4, 4, 2), 2, 2, 2);
+
+    sim.printPositions();
+
+    std::cout << l1 -> springs.size() << std::endl;
+
+    sim.setSpringConstant(1000);
     sim.setMassDeltaT(0.0001);
 
     sim.createPlane(Vec(0, 0, 1), 0);
 
 #ifdef GRAPHICS
     sim.setBreakpoint(40);
-//    sim.runFunc(translateMass, 1.0);
-
     sim.run();
 #else
-    sim.setBreakpoint(0.01);
-//    sim.runFunc(translateMass, 1.0);
+    sim.setBreakpoint(0.1);
     sim.run();
 
     while (sim.time() < 1) {
         sim.printPositions();
-        sim.setBreakpoint(sim.time() + 0.01);
+        sim.setBreakpoint(sim.time() + 0.1);
         sim.resume();
     }
 #endif
