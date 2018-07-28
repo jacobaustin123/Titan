fc5b230623027f660db85c82e60b9fb4d38ba7ae
Jacob Austin (Alienware) <jacobaustin123@gmail.com>
2018-07-28 19:01:02 -0400

    final dynamic libraries

--- vcpkg/portfile.cmake
+++ vcpkg/portfile.cmake
@@ -17,38 +17,69 @@ include(vcpkg_common_functions)
 #    FILENAME "zlib1211-2.zip"
 #    SHA512 9069fe4a9bfae1d45bcf000404c9f89605741b67987b8ca412b84ba937ed1b7bba4b8b174b6f9bc6814776def5f4b34ec7785cd84aa410002d87ddd1b507f11a
 #)
-#vcpkg_extract_source_archive(${ARCHIVE})
 
-set(SOURCE_PATH /Users/JAustin/Desktop/Loch)
+vcpkg_extract_source_archive(${VCPKG_ROOT_DIR}/downloads/Loch-package.zip)
+
+#vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+#        REPO ja3067/Loch
+#        REF 70a6c2217f1b5dd199123e419e51853fe7a290cc
+#        SHA512 1
+#        HEAD_REF package
+#        )
+
+#vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+#        REPO Microsoft/vcpkg
+#        REF 2035d0124dd01ea3ebabfdcc99c7275f723cab13
+#        SHA512 8291639d16ebf9f5394f601b7951028dbb095dfece47c57deb1b2e43f5a893c0b128632d82157bfe1f0ce26f21837a598a638cc399e16d1906d825a3ed5e10ac
+#        HEAD_REF master
+#        )
+
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/Loch-package)
+
+find_program(NVCC
+        NAMES nvcc nvcc.exe
+        PATHS
+        ENV CUDA_PATH
+        ENV CUDA_BIN_PATH
+        PATH_SUFFIXES bin bin64
+        DOC "Toolkit location."
+        NO_DEFAULT_PATH
+        )
+
+set(CMAKE_CUDA_COMPILER:FILEPATH ${NVCC})
+
+#message(STATUS ${NVCC})
+
+#message(STATUS ${TARGET_TRIPLET} ${VCPKG_TARGET_ARCHITECTURE})
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  if (${TARGET_TRIPLET} STREQUAL "x64-windows-static")
-    message(FATAL_ERROR "Loch currently does not support static linkage on Windows. Please use the x64-windows triplet!")
-  else()
+    message(FATAL_ERROR "The Loch library does not currently support static compilation. Please use the x64-windows or x64-osx triplets")
+
     vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS
-        -DLOCH_SHARED_BUILD=OFF
+            SOURCE_PATH ${SOURCE_PATH}
+            PREFER_NINJA
+            OPTIONS
+            -DLOCH_SHARED_BUILD=OFF
+            -DCMAKE_CUDA_COMPILER:FILEPATH=${NVCC}
     )
-  endif()
 else()
     message(STATUS ${VCPKG_LIBRARY_LINKAGE})
     vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS
-        -DLOCH_SHARED_BUILD=ON
+            SOURCE_PATH ${SOURCE_PATH}
+            PREFER_NINJA
+            OPTIONS
+            -DLOCH_SHARED_BUILD=ON
+            -DCMAKE_CUDA_COMPILER:FILEPATH=${NVCC}
     )
 endif()
 
 
 vcpkg_install_cmake()
 file(
-    REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+        REMOVE_RECURSE
+        ${CURRENT_PACKAGES_DIR}/debug/include
+        ${CURRENT_PACKAGES_DIR}/debug/share
 )
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/copyright.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/loch RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/copyright.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/lochgpu RENAME copyright)
