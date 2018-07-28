45c032f3f1c52fc5d222f46099459195561fd41c
Jacob Austin (Alienware) <jacobaustin123@gmail.com>
2018-07-28 13:01:55 -0400

    working on windows

---
+++ cmake-build-release/CMakeFiles/Loch.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Loch.dir/src/vec.cpp.obj"
+  "CMakeFiles/Loch.dir/src/sim.cpp.obj"
+  "CMakeFiles/Loch.dir/src/mass.cpp.obj"
+  "CMakeFiles/Loch.dir/src/spring.cpp.obj"
+  "CMakeFiles/Loch.dir/src/object.cpp.obj"
+  "CMakeFiles/Loch.dir/src/graphics.cpp.obj"
+  "Loch.pdb"
+  "Loch.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Loch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
