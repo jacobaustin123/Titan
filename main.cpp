7a411fd808b158f572b1a082faeff0f56bbc4ce6
CreativeMachinesDesktop <creativemachinesdesktop@columbia.edu>
2018-08-13 16:23:27 -0400

    experimental final

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main() {
     Simulation sim;
 
-    Lattice * l2 = sim.createLattice(Vec(0, 0, 10), Vec(5, 5, 5), 10, 10, 10);
-//    Container * l2 = sim.importFromSTL("C:/Users/CreativeMachines/Desktop/Part3.STL", 10, 5); // 10 points per square meter, 5 rays per point.
+//    Lattice * l2 = sim.createLattice(Vec(0, 0, 10), Vec(5, 5, 5), 10, 10, 10);
+    Container * l2 = sim.importFromSTL("C:/Users/CreativeMachines/Desktop/Part3.STL", 10, 5); // 10 points per square meter, 5 rays per point.
 //    sim.setViewport(Vec(5, -10, 3), Vec(0, 0, 2), Vec(0, 0, 1));
 
     sim.setAllSpringConstantValues(1E5);
@@ -13,8 +13,7 @@ int main() {
     std::cout << sim.masses.size() << " " << sim.springs.size() << std::endl;
     sim.createPlane(Vec(0, 0, 1), 0);
 
-    double runtime = 10.0;
-    sim.setGlobalForce(Vec(0, 0, -0.1));
+    sim.setGlobalForce(Vec(0, 0, -9.8));
 
     sim.start(); // 10 second runtime.
 
