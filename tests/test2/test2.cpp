3a633fe310a0786792681794c8b0078a524ed9e9
Jacob Austin <ja3067@columbia.edu>
2018-11-24 16:44:50 -0500

    updated tests

--- tests/test2/test2.cpp
+++ tests/test2/test2.cpp
@@ -1,10 +1,26 @@
-#include <Titan/sim.h>
+#include <Titan/sim.h> 
 
 int main() {
     Simulation sim;
 
-    Lattice * l1 = sim.createLattice(Vec(0, 0, 10), Vec(5, 5, 5), 10, 10, 10);
-    sim.createBall(Vec(0, 0, 0), 2);
+    Lattice * l2 = sim.createLattice(Vec(0, 0, 10), Vec(5, 5, 5), 10, 10, 10);
+    sim.setViewport(Vec(5, -10, 3), Vec(0, 0, 2), Vec(0, 0, 1));
+
+    sim.setAllSpringConstantValues(1E5);
+    l2 -> rotate(Vec(0, 0, 1), -0.78);
+
+    std::cout << sim.masses.size() << " " << sim.springs.size() << std::endl;
+    sim.createPlane(Vec(0, 0, 1), 0);
+
+    sim.setGlobalAcceleration(Vec(0, 0, -9.8));
 
     sim.start(); // 10 second runtime.
+
+    while (true) {
+        sim.pause(sim.time() + 1);
+        sim.get(l2);
+        l2 -> rotate(Vec(0, 0, 1), 0.5);
+        sim.set(l2);
+        sim.resume();
+    }
 }
