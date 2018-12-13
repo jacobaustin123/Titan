27144525fc7f1ce44748c76ad02c4b996b67af16
Tingkai Liu <tl2747@columbia.edu>
2018-12-13 11:24:47 -0500

    added robot object

--- src/object.cu
+++ src/object.cu
@@ -281,10 +281,10 @@ Beam::Beam(const Vec & center, const Vec & dims, int nx, int ny, int nz) {
     for (int i = 0; i < nx; i++) {
         for (int j = 0; j < ny; j++) {
             for (int k = 0; k < nz; k++) {
-                masses.push_back(new Mass(Vec((nx > 1) ? (double) i / (nx - 1.0) - 0.5 : 0, (ny > 1) ? j / (ny - 1.0) - 0.5 : 0, (nz > 1) ? k / (nz - 1.0) - 0.5 : 0) * dims + center));
-                if (i == 0) {
-                    masses[masses.size() - 1] -> constraints.fixed = true;
-                }
+	      masses.push_back(new Mass(Vec((nx > 1) ? (double) i / (nx - 1.0) - 0.5 : 0, (ny > 1) ? j / (ny - 1.0) - 0.5 : 0, (nz > 1) ? k / (nz - 1.0) - 0.5 : 0) * dims + center));
+	      if (i == 0) {
+		masses[masses.size() - 1] -> constraints.fixed = true;
+	      }
             }
         }
     }
@@ -332,6 +332,138 @@ Beam::Beam(const Vec & center, const Vec & dims, int nx, int ny, int nz) {
         }
     }
 
+    for (Spring * s : springs) {
+        s -> setRestLength((s -> _right -> pos - s -> _left -> pos).norm());
+    }
+}
+
+
+Robot::Robot(const Vec & center, const cppn& encoding, double side_length,  double omega, double k_soft, double k_stiff){
+    _center = center;
+    _side_length = side_length;
+    _omega = omega;
+    _k_soft = k_soft;
+    _k_stiff = k_stiff;
+    _encoding = encoding;
+    
+    int RobotDim = encoding.size(); // number of cubes per side
+    Vec dims(side_length,side_length,side_length);
+    // keep trace of number of cubes that each mass is connected to 
+    std::vector<std::vector<std::vector<int>>> mass_conn(RobotDim+1, std::vector<std::vector<int>>(RobotDim+1,std::vector<int>(RobotDim+1,0)));
+    
+    std::vector<std::vector<std::vector<Mass *>>> _masses(RobotDim+1, std::vector<std::vector<Mass *>>(RobotDim+1,std::vector<Mass *>(RobotDim+1,nullptr)));
+  
+    // store number of cubes that should be connected to each mass
+    for (int i = 0; i < RobotDim+1; i++) {
+      for (int j = 0; j < RobotDim+1; j++) {
+	for (int k = 0; k < RobotDim+1; k++) {
+	  // if index mode RobotDim+1 is 0, then it is on the edge
+	  int i_edge = (i % (RobotDim)) ? 0:1; 
+	  int j_edge = (j % (RobotDim)) ? 0:1;
+	  int k_edge = (k % (RobotDim)) ? 0:1;
+
+	
+	  if (i_edge + j_edge + k_edge ==0){
+	    mass_conn[i][j][k] = 8; //corner
+	  }else if (i_edge+j_edge+k_edge ==3){
+	    mass_conn[i][j][k] = 1; //corner
+	  }else if (i_edge+j_edge+k_edge ==2){
+	    mass_conn[i][j][k] = 2; //edge
+	  }else{
+	    mass_conn[i][j][k] = 4; //surface
+	  }	
+	}
+      }
+    }
+
+    // Remove appropriate masses
+    for (int i = 0; i < RobotDim; i++) {
+      for (int j = 0; j < RobotDim; j++) {
+	for (int k = 0; k < RobotDim; k++) {
+	
+	  int exist = encoding[i][j][k][0];
+
+	  if (!exist){
+	    // subtract connectedness of each mass for the cube
+	    mass_conn[i][j][k] -= 1;
+	    mass_conn[i][j][k+1] -= 1;
+	    mass_conn[i][j+1][k] -= 1;
+	    mass_conn[i][j+1][k+1] -= 1;
+	    mass_conn[i+1][j][k] -= 1;
+	    mass_conn[i+1][j][k+1] -= 1;
+	    mass_conn[i+1][j+1][k] -= 1;
+	    mass_conn[i+1][j+1][k+1] -= 1;
+	  }
+	}
+      }
+    }
+  
+    // create masses
+    for (int i = 0; i < RobotDim+1; i++) {
+      for (int j = 0; j < RobotDim+1; j++) {
+	for (int k = 0; k < RobotDim+1; k++) {
+	  if (mass_conn[i][j][k]>0){
+	    Mass * m;
+	    if (RobotDim==1){
+	      m = new Mass(Vec(i-0.5,j-0.5,k-0.5) * dims + _center);
+	    }else{
+	      
+	      m= new Mass(Vec(i / (RobotDim - 1.0) - 0.5,
+			      j / (RobotDim - 1.0) - 0.5,
+			      k / (RobotDim - 1.0) - 0.5) * dims + _center);
+	    }
+	    masses.push_back(m);
+	    _masses[i][j][k] = m;
+	  }
+	}
+      }
+    }
+
+
+    // create springs
+    for (int i = 0; i < RobotDim; i++) {
+      for (int j = 0; j < RobotDim; j++) {
+	for (int k = 0; k < RobotDim; k++) {
+	
+	  int exist = encoding[i][j][k][0];
+
+	  if (exist){
+	    int type = encoding[i][j][k][1];
+	  
+	    for(int l=0; l<8; l++){
+	      int l_x = (l<4)? 0:1;
+	      int l_y = (l<2)? 0:(l<4)?1:(l<6)?0:1;
+	      int l_z = (l%2)? 1:0;
+	    
+	      for(int m=l+1; m<8; m++){
+		int r_x = (m<4)? 0:1;
+		int r_y = (m<2)? 0:(m<4)?1:(m<6)?0:1;
+		int r_z = (m%2)? 1:0;
+
+		Spring * spr = new Spring(_masses[i+l_x][j+l_y][k+l_z],
+					  _masses[i+r_x][j+r_y][k+r_z]);
+		
+		spr -> _type = type;
+		spr -> _omega = omega;
+		if (type==0){// green, contract then expand
+		  spr -> _k = k_soft;
+		}else if(type==1){ // red, expand then contract
+		  spr -> _k = k_soft;
+		}else if(type==2){ // passive soft
+		  spr -> _k = k_soft;
+		}else{ // passive stiff
+		  spr -> _k = k_stiff;
+		}
+		
+		springs.push_back(spr);
+	      }
+	    }
+	  }
+	}
+      }
+    }
+
+    
     for (Spring * s : springs) {
         s -> setRestLength((s -> _right -> pos - s -> _left -> pos).norm());
     }
