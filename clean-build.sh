2d5d3d9e6ab3c197a2c128aaf5a722d2b12b7935
Jacob Austin <ja3067@columbia.edu>
2020-05-22 12:05:24 -0400

    updated install scripts

--- clean-build.sh
+++ clean-build.sh
@@ -35,3 +35,11 @@ fi
 rm -rf *
 cmake ../../ "${args[@]}"
 cmake --build . -- -j12
+
+read -p "Do you want to install Titan globally? [y/N]" -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo make install
+    sudo chmod 755 /usr/local
+fi
